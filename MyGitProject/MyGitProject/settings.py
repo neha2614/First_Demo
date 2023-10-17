40eb6055276fe172050536a7dc6986a749b64dc9
neha2614 <nehapedgaonkar2@gmail.com>
2023-10-17 22:42:06 +0530

    Updated template path

--- MyGitProject/MyGitProject/settings.py
+++ MyGitProject/MyGitProject/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'MyGitProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['LearnGit/Templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
